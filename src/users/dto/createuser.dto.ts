e537d98286a2e97585629ec037e025acb7f6d50e
Carlos Vinicius <carlosestudante82675@gmail.com>
2025-07-02 18:29:15 -0300

    feature: user-DTO

---
+++ src/users/dto/createuser.dto.ts
@@ -0,0 +1,34 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength, MinLength } from "class-validator";
+import { UserRole } from "src/enums/user-role.enum";
+
+export class UserDTO {
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(68)
+    nomeDeUsuario: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @MaxLength(14)
+    @MinLength(8)
+    senha: string;
+
+    @IsOptional()
+    @IsEnum(UserRole)
+    role?: UserRole;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(600)
+    bio?: string;
+
+    @IsOptional()
+    @IsUrl()
+    avatar?: string;
+
+
+}
