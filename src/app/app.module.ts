eae5f55a2fff9d4c7bc96f433fd8599f3d803e17
Carlos Vinicius <carlosestudante82675@gmail.com>
2025-07-10 15:13:36 -0300

    fix: declare authModule/appModule-exports:UserService/userModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from 'src/users/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
 
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1/Wikitech'),
     UserModule, //Modulo user
+    AuthModule,
     //tornando o ConfigModule acessivel
     ConfigModule.forRoot({
       isGlobal: true, 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,4 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { User } from "src/schemas/user.schema";
 import { LoginDto } from "./dto/login.dto";
 import { UserService } from "src/users/user.service";
 import * as bcrypt from 'bcrypt';
@@ -13,7 +10,6 @@ export class AuthService {
 
 
 constructor (
-    @InjectModel(User.name) private UserModel: Model<User>,
     private userService: UserService,
     private jwtService: JwtService, 
 ){}
