c612abef498b549e428c0abe1e34ead233d00b1a
Carlos Vinicius <carlosestudante82675@gmail.com>
2025-07-10 15:40:00 -0300

    Guards/roles/decorator

---
+++ src/auth/current-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+
+//permite acessar facilmente o usuário autenticado dentro de um controller.
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user; // Definido pelo JwtStrategy
+  },
+);
