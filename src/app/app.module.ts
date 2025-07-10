d6dc14594f20e60afa98a8b6ac5f59ed2dce4db0
Carlos Vinicius <carlosestudante82675@gmail.com>
2025-07-10 14:53:11 -0300

    auth/module-app/module-env-nestjs/config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from 'src/users/user.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1/Wikitech'),
     UserModule, //Modulo user
+    //tornando o ConfigModule acessivel
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,8 @@ import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { UserModule } from "src/users/user.module";
-import { JwtModule } from "@nestjs/jwt";
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from "./jwt.strategy";
 
 
@@ -11,9 +12,16 @@ import { JwtStrategy } from "./jwt.strategy";
 imports:[
    UserModule,
    //configurando e declarando a chave dos tokens
-   JwtModule.register({
-      secret: 'bungas',
-      signOptions: { expiresIn: '1h' },
+   JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+        //vai ler do .env
+    secret: configService.get<string>('JWT_SECRET'),
+    signOptions: {
+      expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+            },
+        }),
     }),
 ],
 
