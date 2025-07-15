aee7d25621e4a4af603446db91c3cdb95f44f75a
Carlos Vinicius <carlosestudante82675@gmail.com>
2025-07-15 19:18:34 -0300

    Create-PostController/PostModule

---
+++ src/post/post.controller.ts
@@ -0,0 +1,70 @@
+import { Body, Controller, Delete, Get, HttpException, Param, Patch, Post, UseGuards } from "@nestjs/common";
+import { PostService } from "./post.service";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { CreatePostDTO } from "./Postdto/create-post.dto";
+import { CurrentUser } from "src/auth/current-user.decorator";
+import { JwtPayload } from "src/auth/jwt-payload.interface";
+import mongoose from "mongoose";
+import { UpdatePostDTO } from "./Postdto/update-post.dto";
+
+
+
+@Controller('post')
+
+export class PostController{
+
+    constructor(private postService: PostService){}
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async createPost(@Body() post:CreatePostDTO, @CurrentUser() user: JwtPayload){
+
+        return this.postService.createPost(post,user.sub);
+    }
+
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async getPosts(){
+
+        return this.postService.listAllPosts();
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':id')
+    async findPost(@Param('id')id: string){
+
+        //verifico se o id passado é um objeto valido
+        const isValid = mongoose.Types.ObjectId.isValid(id) // melhor em um middleware
+        if(!isValid) throw new HttpException('post invalido', 404);
+
+        return this.postService.findAPost(id);
+
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch(':id')
+    async editPost(@Param('id')id:string, @Body() updatePost: UpdatePostDTO){
+
+        const isValid = mongoose.Types.ObjectId.isValid(id) // melhor em um middleware
+        if(!isValid) throw new HttpException('post invalido', 404);
+
+        return this.postService.editPost(updatePost,id);
+
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Delete(':id')
+    async deletePost(@Param('id') id: string){
+
+        const isValid = mongoose.Types.ObjectId.isValid(id) // melhor em um middleware
+        if(!isValid) throw new HttpException('post invalido', 404);
+
+        return this.postService.deletePost(id);
+    }
+
+
+
+
+
+}
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Post, PostSchema } from "src/schemas/post.schema";
 import { PostService } from "./post.service";
+import { PostController } from "./post.controller";
 
 
 @Module({
@@ -18,7 +19,9 @@ providers: [
     PostService,
 ],
 
-controllers: [],
+controllers: [
+    PostController,
+],
 
 exports: [],
 
