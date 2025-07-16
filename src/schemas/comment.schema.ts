8e7c9f5fe388e4428f45a7b7070a96ff986896b6
Carlos Vinicius <carlosestudante82675@gmail.com>
2025-07-15 22:56:29 -0300

    Comment-Schema

--- src/schemas/comment.schema.ts
+++ src/schemas/comment.schema.ts
@@ -1,9 +1,22 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
+
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { mongo, Types } from "mongoose";
 
 
-@Schema()
+@Schema({timestamps: true})
 export class Comment{
 
+@Prop({required: true})
+conteudo: string;
+
+@Prop({type: mongoose.Schema.Types.ObjectId, ref: 'User',required: true})
+autor: Types.ObjectId;
+
+@Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true})
+post: Types.ObjectId;
+
+@Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Comment', default: null})
+comentariopai?: Types.ObjectId;
 
 
 
